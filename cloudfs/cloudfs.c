3ed701d4315fb9626f8fd1bd5757f64e13c1f903
Yinsu Chu <chuyinsu@gmail.com>
2014-03-29 00:52:13 -0400

    Add cloudfs_getattr().

--- cloudfs/cloudfs.c
+++ cloudfs/cloudfs.c
@@ -1,3 +1,11 @@
+/**
+ * @file cloudfs.c
+ * @brief 15-746 Spring 2014 Project 2 - Hybrid Cloud Storage System
+ * @author Yinsu Chu (yinsuc)
+ */
+
+#define _XOPEN_SOURCE 500 /* for blksize_t */
+
 #include <ctype.h>
 #include <dirent.h>
 #include <errno.h>
@@ -20,30 +28,85 @@
 
 #define UNUSED __attribute__((unused))
 
+/* a simple debugging utility,
+ * uncomment the next line to display debugging information */
+#define DEBUG
+#ifdef DEBUG
+# define dbg_print(...) printf(__VA_ARGS__)
+#else
+# define dbg_print(...) 
+#endif
+
+/* to check error return of many function calls in a row,
+ * such as in cloudfs_getattr(). */
+#define CK_ERR(f, m) \
+  if ((f) < 0) { \
+    retval = cloudfs_error(m); \
+  }
+
+/* extended attributes in the user namespace */
+#define U_DEV ("user.st_dev")
+#define U_INO ("user.st_ino")
+#define U_MODE ("user.st_mode")
+#define U_NLINK ("user.st_nlink")
+#define U_UID ("user.st_uid")
+#define U_GID ("user.st_gid")
+#define U_RDEV ("user.st_rdev")
+#define U_SIZE ("user.st_size")
+#define U_BLKSIZE ("user.st_blksize")
+#define U_BLOCKS ("user.st_blocks")
+#define U_ATIME ("user.st_atime")
+#define U_MTIME ("user.st_mtime")
+#define U_CTIME ("user.st_ctime")
+
 static struct cloudfs_state state_;
 
-static int UNUSED cloudfs_error(char *error_str)
+/**
+ * @brief Check whether a file is stored in the cloud.
+ *        This is marked by extended attribute "user.remote".
+ * @param fpath Full path of the file on SSD. 
+ * @return 1 if is in the cloud, 0 otherwise.
+ */
+static int cloudfs_is_in_cloud(char *fpath)
 {
-    int retval = -errno;
-
-    // TODO:
-    //
-    // You may want to add your own logging/debugging functions for printing
-    // error messages. For example:
-    //
-    // debug_msg("ERROR happened. %s\n", error_str, strerror(errno));
-    //
-    
-    fprintf(stderr, "CloudFS Error: %s\n", error_str);
-
-    /* FUSE always returns -errno to caller (yes, it is negative errno!) */
-    return retval;
+  int retval = 0;
+  lgetxattr(fpath, "user.remote", &retval, sizeof(int));
+  dbg_print("[DBG] cloudfs_is_in_cloud(fpath=\"%s\")=%d\n", fpath, retval);
+  return retval;
 }
 
-/*
- * Initializes the FUSE file system (cloudfs) by checking if the mount points
- * are valid, and if all is well, it mounts the file system ready for usage.
- *
+/**
+ * @brief Get the fullpath in the underlying filesystem (SSD)
+ *        of a given CloudFS path.
+ * @param path A CloudFS path.
+ * @param fullpath The "real" path in SSD is returned here. It should
+ *                 have at least MAX_PATH_LEN byte space.
+ * @return Void.
+ */
+void cloudfs_get_fullpath(const char *path, char *fullpath)
+{
+  memset(fullpath, '\0', MAX_PATH_LEN);
+  snprintf(fullpath, MAX_PATH_LEN, "%s%s", state_.ssd_path, path);
+  dbg_print("[DBG] cloudfs_get_fullpath(path=\"%s\", fullpath=\"%s\")",
+      path, fullpath);
+}
+
+/**
+ * @brief Translate errno to FUSE error return value by negating it.
+ * @param error_str The error message passed from the caller.
+ * @return FUSE error return value (-errno).
+ */
+static int cloudfs_error(char *error_str)
+{
+  int retval = -errno;
+  dbg_print("[ERR] %s : %s\n", error_str, strerror(errno));
+  return retval;
+}
+
+/**
+ * @brief Initializes the FUSE file system (cloudfs) by checking
+ *        if the mount points are valid, and if all is well,
+ *        it mounts the file system ready for usage.
  */
 void *cloudfs_init(struct fuse_conn_info *conn UNUSED)
 {
@@ -55,15 +118,47 @@ void cloudfs_destroy(void *data UNUSED) {
   cloud_destroy();
 }
 
-int cloudfs_getattr(const char *path UNUSED, struct stat *statbuf UNUSED)
+/**
+ * @brief Get file attributes.
+ *        For files stored on SSD, just retrieve the attributes directly;
+ *        for files in the cloud, refer to the extended attributes
+ *        of the proxy file.
+ * @param path Pathname of the file.
+ * @param sb Buffer for struct stat. The returned information is placed here.
+ * @return 0 on success, -1 otherwise.
+ */
+int cloudfs_getattr(const char *path, struct stat *sb)
 {
   int retval = 0;
+  char fpath[MAX_PATH_LEN] = "";
+  char *fn = "cloudfs_getattr";
 
-  // 
-  // TODO:
-  //
-  // Implement this function to do whatever it is supposed to do!
-  //
+  dbg_print("[DBG] cloudfs_getattr(path=\"%s\", sb=0x%08x)\n",
+      path, (unsigned int)sb);
+
+  cloudfs_get_fullpath(path, fpath);
+
+  memset(sb, '\0', sizeof(struct stat));
+  if (cloudfs_is_in_cloud(fpath)) {
+    CK_ERR(lstat(fpath, sb), fn);
+    CK_ERR(lgetxattr(fpath, U_DEV, &sb->st_dev, sizeof(dev_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_INO, &sb->st_ino, sizeof(ino_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_MODE, &sb->st_mode, sizeof(mode_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_NLINK, &sb->st_nlink, sizeof(nlink_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_UID, &sb->st_uid, sizeof(uid_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_GID, &sb->st_gid, sizeof(gid_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_RDEV, &sb->st_rdev, sizeof(dev_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_SIZE, &sb->st_size, sizeof(off_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_BLKSIZE, &sb->st_blksize, sizeof(blksize_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_BLOCKS, &sb->st_blocks, sizeof(blkcnt_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_ATIME, &sb->st_atime, sizeof(time_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_MTIME, &sb->st_mtime, sizeof(time_t)), fn);
+    CK_ERR(lgetxattr(fpath, U_CTIME, &sb->st_ctime, sizeof(time_t)), fn);
+  } else {
+    if (lstat(fpath, sb) < 0) {
+      retval = cloudfs_error(fn);
+    }
+  }
 
   return retval;
 }
@@ -73,27 +168,27 @@ int cloudfs_getattr(const char *path UNUSED, struct stat *statbuf UNUSED)
  */
 static 
 struct fuse_operations cloudfs_operations = {
-    .init           = cloudfs_init,
-    //
-    // TODO
-    //
-    // This is where you add the VFS functions that your implementation of
-    // MelangsFS will support, i.e. replace 'NULL' with 'melange_operation'
-    // --- melange_getattr() and melange_init() show you what to do ...
-    //
-    // Different operations take different types of parameters. This list can
-    // be found at the following URL:
-    // --- http://fuse.sourceforge.net/doxygen/structfuse__operations.html
-    //
-    //
-    .getattr        = NULL,
-    .mkdir          = NULL,
-    .readdir        = NULL,
-    .destroy        = cloudfs_destroy
+  .init           = cloudfs_init,
+  //
+  // TODO
+  //
+  // This is where you add the VFS functions that your implementation of
+  // MelangsFS will support, i.e. replace 'NULL' with 'melange_operation'
+  // --- melange_getattr() and melange_init() show you what to do ...
+  //
+  // Different operations take different types of parameters. This list can
+  // be found at the following URL:
+  // --- http://fuse.sourceforge.net/doxygen/structfuse__operations.html
+  //
+  //
+  .getattr        = cloudfs_getattr,
+  .mkdir          = NULL,
+  .readdir        = NULL,
+  .destroy        = cloudfs_destroy
 };
 
 int cloudfs_start(struct cloudfs_state *state,
-                  const char* fuse_runtime_name) {
+    const char* fuse_runtime_name) {
 
   int argc = 0;
   char* argv[10];
@@ -107,6 +202,6 @@ int cloudfs_start(struct cloudfs_state *state,
   state_  = *state;
 
   int fuse_stat = fuse_main(argc, argv, &cloudfs_operations, NULL);
-    
+
   return fuse_stat;
 }
